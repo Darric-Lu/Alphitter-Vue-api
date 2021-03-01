1a003117f47a84ff4134e6030bfec3d0a3d23892
Claire <s3310507057@gmail.com>
2021-03-01 23:17:13 +0800

    install fontawesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
+
 
 Vue.config.productionTip = false
+
+library.add(faHome, faUser, faCog, faSignInAlt)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
