bf2c15dd09e92a98b765634c37ae628988703807
Claire <s3310507057@gmail.com>
2021-03-01 18:28:47 +0800

    add pages framwork

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
-import SignIn from '../views/SignIn'
-import SignUp from '../views/SignUp'
-import AdminSignIn from '../views/AdminSignIn'
+import SignIn from '../views/SignIn.vue'
+import SignUp from '../views/SignUp.vue'
+import AdminSignIn from '../views/AdminSignIn.vue'
+import TweetMain from '../views/TweetMain.vue'
+import ReplyList from '../views/TweetReplyList.vue'
+import Userself from '../views/Userself.vue'
+import UserselfLike from '../views/UserselfLike.vue'
+import UserselfFollower from '../views/UserselfFollower.vue'
+import UserselfFollowing from '../views/UserselfFollowing.vue'
+import UserOther from '../views/UserOther.vue'
 
 Vue.use(VueRouter)
 
@@ -22,6 +29,41 @@ const routes = [
     path:'/admin/signin',
     name: 'admin-sign-in',
     component: AdminSignIn
+  },
+  {
+    path:'/main',
+    name: 'main',
+    component: TweetMain
+  },
+  {
+    path:'/reply_list',
+    name: 'reply-list',
+    component: ReplyList
+  },
+  {
+    path:'/user/self',
+    name: 'user-self',
+    component: Userself
+  },
+  {
+    path:'/user/self/like',
+    name: 'user-self-like',
+    component: UserselfLike
+  },
+  {
+    path:'/user/self/follower',
+    name: 'user-self-follower',
+    component: UserselfFollower
+  },
+  {
+    path:'/user/self/following',
+    name: 'user-self-following',
+    component: UserselfFollowing
+  },
+  {
+    path:'/user/other',
+    name: 'user-other',
+    component: UserOther
   },
   {
     path: '*',
---
+++ src/views/UserselfLike.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-2 left-col">
+        <!-- Sidebar -->
+      </div>
+      <div class="col-lg-6 mid-col">
+        <div class="mid-up">
+          <!-- UserNavbar -->
+          <!-- Userprofile -->
+        </div>
+        <div class="mid-down">
+          <!-- twitterCardTable -->
+        </div>
+      </div>
+      <div class="col-lg-3 right-col">
+        <!-- Recommendationtable -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 15px;
+  padding-left: 15px;
+  width: 100%;
+}
+.row {
+  display: flex;
+  border: 1px solid crimson;
+}
+.left-col {
+  border: 1px solid blue;
+  width: 235px;
+  height: 1196px;
+}
+.mid-col {
+  display: flex;
+  flex-flow: column wrap;
+  border: 1px solid blue;
+  margin-right: auto;
+  margin-left: auto;
+  width: 600px;
+  height: 1202px;
+}
+.right-col {
+  border: 1px solid blue;
+  width: 350px;
+  height: 517px;
+}
+.mid-up {
+  border: 1px solid green;
+  width: 100%;
+  height: 400px;
+}
+.mid-down {
+  border: 1px solid black;
+  margin-top: 10px;
+  width: 100%;
+  height: 600px;
+}
+</style>
