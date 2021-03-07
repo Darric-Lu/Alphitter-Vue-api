5d0c6a2a6246b81a6f368e2e50869090fa74d383
Claire <s3310507057@gmail.com>
2021-03-08 02:34:49 +0800

    optimized tab layout

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="tab">
-    <router-link to="/admin/users">推文</router-link>
-    <router-link to="/admin/users">推文與回覆</router-link>
-    <router-link to="/admin/users">喜歡的內容</router-link>
+    <div class="tab-link"><router-link to="/user/self">推文</router-link></div>
+    <div class="tab-link"><router-link to="/user/self/tweetsandreplies">推文與回覆</router-link></div>
+    <div class="tab-link"><router-link to="/user/self/like">喜歡的內容</router-link></div>
   </div>
 </template>
 
@@ -11,5 +11,21 @@
   display: flex;
   justify-content: space-around;
   width: 390px;
+}
+.tab-link {
+  display: flex;
+  justify-content: center;
+  width: 130px;
+}
+.tab-link:hover {
+  border-bottom: 2px solid #ff6600;
+}
+a {
+  color: #657786;
+  text-decoration: none;
+  font-weight: bold;
+}
+a:hover {
+  color: #ff6600;
 }
 </style>
