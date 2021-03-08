e336a05c12d09421874e984bfff2c4da5d42fa50
Darric <darric.lu@gmail.com>
2021-03-09 02:31:35 +0800

    Tab opmtimize

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="tab">
+  <div class="tab mt-5">
     <div class="tab-link"><router-link to="/user/self">推文</router-link></div>
-    <div class="tab-link"><router-link to="/user/self/tweetsandreplies">推文與回覆</router-link></div>
-    <div class="tab-link"><router-link to="/user/self/like">喜歡的內容</router-link></div>
+    <div class="tab-link">
+      <router-link to="/user/self/tweetsandreplies">推文與回覆</router-link>
+    </div>
+    <div class="tab-link">
+      <router-link to="/user/self/like">喜歡的內容</router-link>
+    </div>
   </div>
 </template>
 
@@ -11,11 +15,13 @@
   display: flex;
   justify-content: space-around;
   width: 390px;
+  border-bottom: 2px solid #e6ecf0;
 }
 .tab-link {
   display: flex;
   justify-content: center;
   width: 130px;
+  padding-bottom: 15px;
 }
 .tab-link:hover {
   border-bottom: 2px solid #ff6600;
